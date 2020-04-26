118a08f889ba332e301cf8afbbe1d154a6163806
Artem Drei <drei.artem@pdffiller.team>
2020-04-26 14:54:22 +0300

    Change styles for a grid

--- src/containers/Tutorial/index.tsx
+++ src/containers/Tutorial/index.tsx
@@ -8,17 +8,15 @@ import s from './styles.scss';
 
 const Tutorial: React.FC<IProps> = ({ setIsStarted }) => {
   return (
-    <div>
+    <div className={s.tutorial}>
       <h2 className={s.title}>Find 8</h2>
+      <p>One character is different from the rest.</p>
       <p>
-        <p>One character is different from the rest.</p>
-        <p>
-          Click{' '}
-          <Button variant="link" onClick={setIsStarted} className={s.playBtn}>
-            Play
-          </Button>{' '}
-          and try to find it as quickly as possible
-        </p>
+        Click{' '}
+        <Button variant="link" onClick={setIsStarted} className={s.playBtn}>
+          Play
+        </Button>{' '}
+        and try to find it as quickly as possible
       </p>
     </div>
   );
